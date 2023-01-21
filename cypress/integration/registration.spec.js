7b502b52e5d938f3c9fe9f4ccb97213a7a9ab068
Victor <Vilala593@gmail.com>
2023-01-21 18:42:24 +0300

    расширение тестов регистрации

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -33,4 +33,39 @@ describe('Регистрация', ()=> {
         //добавить проверку disabled кнопки
         cy.contains('Введите настоящий email').should('be.visible') //be.visible - должен быть виден   
     })
+    it('Не успешная регистрация, используя валидный логин и невалидный пароль', ()=>{ 
+        cy.visit('https://stage.deepskills.ru/my-way?action=sign-up') 
+ 
+        const rnd = Math.round(Math.random() * 8999) + 1000; 
+        const username = 'user_' + rnd; 
+        const email = username + 'gmail.com'; 
+ 
+        cy.get('[data-qa="auth-name_input"]').type(username) 
+        cy.get('[data-qa="auth-email_input"]').type(email) 
+        cy.get('[data-qa="auth-phone_input"]').type('+7 000 000 00 00') 
+        cy.get('[data-qa="auth-next_button"]').click() 
+        cy.get('[data-qa="auth-password_input"]').type("Password") 
+        cy.contains('Длина пароля не может быть меньше 6-и').should('be.visible') 
+
+     }) 
+ 
+    it('Не успешная регистрация, используя ввод пустых полей(+по отдельности)', ()=>{ 
+        cy.visit('https://stage.deepskills.ru/my-way?action=sign-up') 
+ 
+        const rnd = Math.round(Math.random() * 8999) + 1000; 
+        const username = 'user_' + rnd; 
+        const email = username + 'gmail.com'; 
+
+        
+ 
+     }) 
+ 
+    it('Перезагрузить страницу с пройденной регистрацией', ()=>{ 
+        cy.visit('https://stage.deepskills.ru/my-way?action=sign-up') 
+ 
+ 
+    }) 
+ 
+ 
 })
+
