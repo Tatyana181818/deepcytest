4ed8159089184bc02e34dd5e3f07068e63b6b229
Tatyana181818 <tanjazukowitsch@gmail.com>
2023-01-21 19:16:04 +0300

    2 test for auth

--- cypress/integration/authorization.spec.js
+++ cypress/integration/authorization.spec.js
@@ -7,4 +7,14 @@ describe('Авторизация', () => {
         cy.url().should('eq', 'https://stage.deepskills.ru/courses')
         cy.get('h1').should('eq', 'Курсы')
     })
+
+    it('Не успешная авторизация, используя невалидный логин и валидный пароль', ()=>{
+    	cy.visit("https://stage.deepskills.ru/my-way?action=sign-in")
+        cy.get('[data-qa="auth-email_input"]').type("nevalidmail@gmail.com")
+        cy.get('[data-qa="auth-password_input"]').type("Johny#97")
+        cy.get('[data-qa="auth-submit_button"]').click()
+        cy.contains('Пользователь с такими данными не найден').should('be.visible')
+       
+    })
+
 })
